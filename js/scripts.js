4df6866cdfb8d37a65889b5dafa47c67b5c8203b
Bobby Martin and Matt Nardoni <bobby@>
2018-03-19 11:45:35 -0700

    Add names for arithmatic in prompts

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ var add = function(number1, number2) {
 	return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var number1 = parseInt(prompt("Enter a number for addition:"));
+var number2 = parseInt(prompt("Enter another number for addition:"));
 
 alert(add(number1, number2));
 
@@ -11,8 +11,8 @@ var subtract = function(number1, number2) {
 	return number1 - number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var number1 = parseInt(prompt("Enter a number for subtraction:"));
+var number2 = parseInt(prompt("Enter another number for subtraction:"));
 
 alert(subtract(number1, number2));
 
@@ -20,8 +20,8 @@ var multiply = function(number1, number2) {
 	return number1 * number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var number1 = parseInt(prompt("Enter a number for multiplication:"));
+var number2 = parseInt(prompt("Enter another number for multiplication:"));
 
 alert(multiply(number1, number2));
 
@@ -29,7 +29,7 @@ var divide = function(number1, number2) {
 	return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var number1 = parseInt(prompt("Enter a number for division:"));
+var number2 = parseInt(prompt("Enter another number for division:"));
 
 alert(divide(number1, number2));
