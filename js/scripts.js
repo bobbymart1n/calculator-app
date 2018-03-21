ae3716ac46893a4c8ce84586d67aa1011f36b45f
Bobby Martin and Cameron Button <bobby@>
2018-03-21 14:59:52 -0700

    working calculator with radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -16,10 +16,21 @@ var divide = function(number1, number2) {
 
 
 $(document).ready(function(){
-	$('form#add').submit(function(event){
+	$('form#calculate').submit(function(event){
+		//debugger;
 		var number1 = parseInt($('#add1').val());
 		var number2 = parseInt($('#add2').val());
-		var result = add(number1, number2);
+		var choiceResult = $("input:radio[name=choice]:checked").val(); //will return which one was checked!
+		console.log(choiceResult);
+		if (choiceResult === "add") {
+			result = add(number1, number2);
+		} else if (choiceResult === "sub") {
+			result = subtract(number1, number2);
+		} else if (choiceResult === "mult") {
+			result = multiply(number1, number2);
+		} else if (choiceResult === "div") {
+			result = divide(number1, number2);
+		}
 		$("#output").text(result);
 		event.preventDefault();
 	});
