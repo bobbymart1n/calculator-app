dc6b41e5ac0dc35bc63db650770b7762ea0a4f1e
Bobby Martin and Cameron Button <bobby@>
2018-03-21 13:30:53 -0700

    Add front end logic to add form

--- js/scripts.js
+++ js/scripts.js
@@ -2,34 +2,25 @@ var add = function(number1, number2) {
 	return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number for addition:"));
-var number2 = parseInt(prompt("Enter another number for addition:"));
-
-alert(add(number1, number2));
-
 var subtract = function(number1, number2) {
 	return number1 - number2;
 };
-
-var number1 = parseInt(prompt("Enter a number for subtraction:"));
-var number2 = parseInt(prompt("Enter another number for subtraction:"));
-
-alert(subtract(number1, number2));
 
 var multiply = function(number1, number2) {
 	return number1 * number2;
 };
 
-var number1 = parseInt(prompt("Enter a number for multiplication:"));
-var number2 = parseInt(prompt("Enter another number for multiplication:"));
-
-alert(multiply(number1, number2));
-
 var divide = function(number1, number2) {
 	return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number for division:"));
-var number2 = parseInt(prompt("Enter another number for division:"));
 
-alert(divide(number1, number2));
+$(document).ready(function(){
+	$('form#add').submit(function(event){
+		var number1 = parseInt($('#add1').val());
+		var number2 = parseInt($('#add2').val());
+		var result = add(number1, number2);
+		$("#output").text(result);
+		event.preventDefault();
+	});
+});
